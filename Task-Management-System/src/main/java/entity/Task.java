fec7e200fb6033a916a12faa1744d6870d759ee2
Sonali <sonalilavate7@gmail.com>
2026-06-11 12:19:21 +0530

    add task entity

---
+++ Task-Management-System/src/main/java/entity/Task.java
@@ -0,0 +1,19 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+}
